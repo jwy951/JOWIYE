b0472c15438d71bc12b9575bb483b03d75ac7f3f
Santa claus <santa@nortpole.com>
2021-06-18 10:37:45 +0300

    add profile content

--- start.sh
+++ start.sh
@@ -1,7 +1,6 @@
-export MAIL_USERNAME='yegon.johnstone@student.moringaschool.co'
+export MAIL_USERNAME='yegon.johnstone@student.moringaschool.com'
 export MAIL_PASSWORD='@Jo30196642'
 export SECRET_KEY='30196642'
-export DATABASE_URL='postgresql+psycopg2://moringa:301966
-1@localhost/blogs'
+export DATABASE_URL='postgresql+psycopg2://moringa:3019661@localhost/blogs'
 
 python3.8 manage.py server
